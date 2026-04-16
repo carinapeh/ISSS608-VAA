d1d620166382eaf3386ae1c8b3429134a96bb35f
carinapeh <carina.peh.2025@smu.edu.sg>
2026-04-16 22:30:31 +0800

    Hands On Exercise 1

--- pacman/R/p_basepath.R
+++
@@ -1,3 +0,0 @@
-p_basepath <- function(){
-    file.path(Sys.getenv("R_HOME"), "library")
-}
--- pacman/R/p_detectOS.R
+++
@@ -1,9 +0,0 @@
-#' Detects Operating System
-#' 
-#' Attempts to detect the operating system.
-#' Returns: "Windows", "Darwin" on Mac, "Linux", or "SunOS" on Solaris
-#' 
-#' @export
-p_detectOS <- function(){
-    Sys.info()[["sysname"]]
-}
